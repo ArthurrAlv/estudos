214c9568454c1e2de9b39b1fb2b3b2a01988c096
Lucas Câmara <lfcamara.ti@gmail.com>
2022-05-19 15:46:55 -0300

    remover exercício

--- Exercicio1.java
+++
@@ -1,7 +0,0 @@
-public class Exercicio1 {
-    
-    public static void main(String[] args) {
-        // Faça um programa que receba um valor que é o valor pago, um segundo valor que é o preço do produto e imprima o troco a ser dado.
-        // obs.: Use os dados através do parâmetro "args"
-    }
-}
