d997014e0b3caa83a7566bc64a8251f37fd5d229
Lucas Câmara <lfcamara.ti@gmail.com>
2022-05-19 15:34:41 -0300

    Incluir exercício média

---
+++ media/MainMedia.java
@@ -0,0 +1,7 @@
+public class MainMedia {
+    
+    public static void main(String[] args) {
+        // Faça um programa que utilize uma estrutura de repetição para ler 50 números armazenando-os em um vetor e calcule e exiba a média aritmética deles. 
+        // Em seguida o programa deve apresentar todos os valores armazenados no vetor que sejam menores que a média.
+    }
+}
