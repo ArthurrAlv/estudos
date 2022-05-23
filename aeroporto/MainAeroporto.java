1b3abbfe7672f91fa8a1ce1e3b6ee5ff3689b38d
Lucas Câmara <lfcamara.ti@gmail.com>
2022-05-23 19:13:35 -0300

    Exercício Aeroporto - Fila

--- aeroporto/MainAeroporto.java
+++ aeroporto/MainAeroporto.java
@@ -4,14 +4,14 @@
  * 
  * Com isso, crie um sistema que:
  * 
- * - Permita o cadastro de clientes armazenando o nome e cpf de cada um.
+ * - Permita o cadastro de clientes armazenando o nome e cpf de cada um. Sistema não deve permitir cadastrar mais de um cliente com o mesmo CPF.
  * - Caso o limite de clientes por vôo seja atingido, o sistema irá incluir o cliente numa fila de espera, para poder ingressar no vôo caso algum outro cliente desista de viajar.
- * - Permita que seja possível aplicar a desistência de um cliente do vôo e, caso haja algum cliente na fila de espera, automaticamente já o inclua no vôo após a desistência.
+ * - Permita que seja possível aplicar a desistência de um cliente do vôo e, caso haja algum cliente na fila de espera, automaticamente já o inclua no vôo após a desistência. A desistência deverá * ser através do CPF do cliente.
  * - Permita a visualização dos clientes do vôo assim como os que estão na fila de espera.
  *
  * Observações:
  * 
- * - Crie uma classe "Voo" que irá manter os clientes do vôo (como um vetor) e terá também a fila de espera.
+ * - Crie uma classe "Voo" que irá manter os clientes do vôo (use um vetor) e terá também a fila de espera.
  * - Crie uma classe "Cliente" para representar os dados do mesmo (nome e cpf - ambos como String).
  * - Crie uma classe "FilaEspera" para representar a fila de espera do vôo.
  *   - é como na vida real, numa fila vc tem a pessoa da frente que é a próxima em relação à vc
@@ -21,6 +21,11 @@
  *      String entrada = JOptionPane.showInputDialog(null, "Texto");
  *   - Para imprimir: 
  *      JOptionPane.showMessageDialog(null, "Texto que será apresentado na janela");
+ * - Em resumo, o sistema deverá ter, a princípio, essas opções:
+ *   1) Novo Passageiro
+ *   2) Desistência
+ *   3) Detalhar Vôo
+ *   0) Sair
  */
 public class MainMedia {
 
